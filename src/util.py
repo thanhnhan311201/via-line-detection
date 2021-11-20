573754c2e13911bfcfc510d18fd107693608f379
Phan Nguyen Thanh Nhan <62050592+thanhnhan311201@users.noreply.github.com>
2021-11-20 13:26:19 +0700

    Update util.py

--- src/util.py
+++ src/util.py
@@ -6,8 +6,10 @@ import numpy as np
 from torch.autograd import Variable
 from torch.autograd import Function as F
 from numpy.polynomial import Polynomial as P
-from parameters import Parameters
-# from src.parameters import Parameters
+try:   
+    from parameters import Parameters
+except:
+    from src.parameters import Parameters
 import math
 
 p = Parameters()
