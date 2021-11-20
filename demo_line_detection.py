b21090c69415683fd244aa874ae3645eb7b7eced
Phan Nguyen Thanh Nhan <62050592+thanhnhan311201@users.noreply.github.com>
2021-11-20 16:53:18 +0700

    Update demo_line_detection.py

--- demo_line_detection.py
+++ demo_line_detection.py
@@ -1,3 +1,4 @@
+import os
 import cv2
 import torch 
 import time
@@ -12,32 +13,48 @@ from src.processing_image import warp_image
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o','--option', type=str, default='image', help="demo line detection on 'image' or 'video', default 'image' ", required=True)
-    parser.add_argument('-d','--direction', type=str, default="", help='direction of demo video', required=True)
+    parser.add_argument('-m','--model_weight', type=str, default='32_tensor(1.1001)_lane_detection_network.pkl', required=True)
+    parser.add_argument('-o','--option', type=str, default='image', help="demo line detection on single 'image', 'folder_images' or 'video', default 'image' ", required=True)
+    parser.add_argument('-d','--direction', type=str, default="", help='direction of demo video')
     parser.add_argument('-s','--save_video', type=bool, default=False)
     args = vars(parser.parse_args())
+
+    epoch, loss = int(a.split('_')[0]), float(a.split('_')[1][7:-1])
     
     net = Net()
     p = Parameters()
-    # load model epoch 34 with total loss is 0.7828
-    net.load_model(1,0.8777)
+    net.load_model(epoch, loss)
 
     # read image from folder images test
     if args['option'] == 'image':
-
         image = cv2.imread(args['direction'])
-
         image_resized = cv2.resize(image,(512,256))
-        cv2.imshow("image",image_resized)
-
+        # cv2.imshow("image",image_resized)
         #x , y are position of points in lines 
         #because previous image is warped -> warp = False
-        x , y = net.predict(image_resized, warp = False)
+        x , y = net.predict(image_resized, warp = True)
         print(x, y)
         image_points_result = net.get_image_points()
-        cv2.imshow("points", image_points_result)
-        cv2.imwrite("result.png",image_points_result)
-        cv2.waitKey()
+        # cv2.imshow("points", image_points_result)
+        cv2.imwrite(f"/image_outputs/res_{args['direction'].split('/')[-1]}",image_points_result)
+        # cv2.waitKey()
+    if args['option'] == 'folder_images':
+        dir = args['direction']
+        img_lst = os.listdir(dir)
+        if not os.path.exists('./res_' + dir):
+          os.mkdir('./res_' + dir)
+
+        for img in img_lst:
+          image = cv2.imread(dir + img)
+          image_resized = cv2.resize(image,(512,256))
+          #x , y are position of points in lines 
+          #because previous image is warped -> warp = False
+          x , y = net.predict(image_resized, warp = True)
+          image_points_result = net.get_image_points()
+          # cv2.imshow("points", image_points_result)
+          
+          cv2.imwrite(f"./res_" + dir + "/res_" + img, image_points_result)
+          # cv2.waitKey()
     if args['option'] == 'video':
         cap = cv2.VideoCapture(args['direction'])
         if args['save_video']:
@@ -45,9 +62,9 @@ if __name__ == "__main__":
             out = cv2.VideoWriter('result-point.avi', fourcc, 30, (512,256))
         while cap.isOpened():
             prevTime = time.time()
-            if not ret:
-                break
             ret, image = cap.read()
+            if not net:
+              break
             t_image = cv2.resize(image,(512,256))
             x , y = net.predict(t_image)
             # fits = np.array([np.polyfit(_y, _x, 1) for _x, _y in zip(x, y)])
@@ -59,11 +76,10 @@ if __name__ == "__main__":
             s = "FPS : "+ str(fps)
             # image_lane = net.get_image_lane()
             cv2.putText(image_points, s, (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0))
-            cv2.imshow("image",image_points)
-            out.write(image_points)
-            
+            # cv2.imshow("image",image_points)
+            if args['save_video']:
+              out.write(image_points)
             key = cv2.waitKey(1)
             if not ret or key == ord('q'):
                 break
-        
         out.release()
